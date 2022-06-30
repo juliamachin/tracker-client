937efb5b89c473e18e71945cdda9301c6c69ba98
juliamachin <juliaamachin@gmail.com>
2022-06-30 14:14:47 -0700

    Created calendar components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import UserLogin from "./Components/UserLogin/UserLogin";
 import CreateUser from "./Components/CreateUser/CreateUser";
+import Calendar from "./Components/Calendar/Calendar";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<UserLogin />} />
+        <Route path="/login" element={<UserLogin />} />
         <Route path="/newuser" element={<CreateUser />} />
+        <Route path="/calendar" element={<Calendar />} />
       </Routes>
     </div>
   );
---
+++ src/Components/Calendar/Calendar.js
@@ -0,0 +1,9 @@
+
+
+const Calendar = () => {
+return (
+    <h1>Calendar page</h1>
+)
+}
+
+export default Calendar
