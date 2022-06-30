989b10134e14d06f94f73e5bda69b62cfd194d54
juliamachin <juliaamachin@gmail.com>
2022-06-30 14:13:31 -0700

    modified form css file

--- src/Components/UserLogin/UserLogin.js
+++ src/Components/UserLogin/UserLogin.js
@@ -3,7 +3,7 @@ import { useRef, useState, useEffect, useContext } from "react";
 import AuthContext from "../../Context/AuthProvider";
 import axios from "../../api/axios";
 
-const LOGIN_URL = "/auth/";
+const LOGIN_URL = "/users/";
 function UserLogin() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
