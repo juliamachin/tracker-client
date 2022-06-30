23a79339537303ae1c45e8eacac20de974981dee
juliamachin <juliaamachin@gmail.com>
2022-06-29 20:12:21 -0700

    moved h1 element into a div

--- src/Components/UserLogin/UserLogin.js
+++ src/Components/UserLogin/UserLogin.js
@@ -59,8 +59,8 @@ function UserLogin() {
         >
           {errorMsg}
         </p>
+        <h1>User Login</h1>
       </div>
-      <h1>User Login</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Username:
